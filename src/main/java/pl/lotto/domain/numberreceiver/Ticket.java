cff098a6b67c48924f5996bbe0ed8ea5ec774e7d
Uzytkownik <maciodan@gmail.com>
2023-08-09 07:55:14 +0200

    complete implementation

--- src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
+++ src/main/java/pl/lotto/domain/numberreceiver/Ticket.java
@@ -2,6 +2,8 @@ package pl.lotto.domain.numberreceiver;
 
 import java.time.LocalDateTime;
 import java.util.Set;
+import lombok.Builder;
 
-record Ticket(String ticketId, LocalDateTime drawDate, Set<Integer> numbersFromUser) {
+@Builder
+record Ticket(String hash, Set<Integer> numbers, LocalDateTime drawDate) {
 }
--- src/main/java/pl/lotto/domain/numberreceiver/dto/TicketDto.java
+++ src/main/java/pl/lotto/domain/numberreceiver/dto/TicketDto.java
@@ -1,9 +1,12 @@
 package pl.lotto.domain.numberreceiver.dto;
 
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 import java.util.Set;
+import lombok.Builder;
+
 @Builder
-public record TicketDto(LocalDateTime drawDate, String ticketId, Set<Integer> numbersFromUser) {
+public record TicketDto(
+        String hash,
+        Set<Integer> numbers,
+        LocalDateTime drawDate) {
 }

fd3af85f465deaebf7d6669f1a2a3fb314dc73a4
Uzytkownik <maciodan@gmail.com>
2023-08-26 14:56:18 +0200

    added bean for adjustable clock

--- src/main/java/pl/lotto/domain/numbergenerator/WinningNumbersRepository.java
+++ src/main/java/pl/lotto/domain/numbergenerator/WinningNumbersRepository.java
@@ -1,10 +1,11 @@
 package pl.lotto.domain.numbergenerator;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
-
+@Repository
 public interface WinningNumbersRepository extends MongoRepository<WinningNumbers, String> {
 
     Optional<WinningNumbers> findNumbersByDate(LocalDateTime date);
